269489b558729867150d1b50599b83841a8cf33e
Chen20191020 <57544179+Chen20191020@users.noreply.github.com>
2026-02-06 13:10:27 -0800

    Add files via upload

---
+++ Table 2/DWT_realdata.R
@@ -0,0 +1,332 @@
+#  classfication for EEG 
+library(wavelets)
+library(MASS)
+library(sparsediscrim)
+
+n = 4096
+k = 2
+N = n/k 
+J = 5
+n_train_1 = 252
+n_train_2 = 252
+n_train_total = n_train_1 + n_train_2
+
+n_test_1 = 75
+n_test_2 = 75
+n_test_total = n_test_1 + n_test_2
+Y = factor(c(rep(1, n_train_1), rep(2, n_train_2)))
+Y_test = factor(c(rep(1, n_test_1), rep(2, n_test_2)))
+
+
+u_dsample <- function(data, target_size = 4096) {
+  n <- length(data)
+  if (n <= target_size) {
+    stop("Input data must have more than 4096 points.")
+  }
+  
+  indices <- round(seq(1, n, length.out = target_size))
+  return(data[indices])
+}
+
+
+# C3 train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_C3.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_C3.rds")
+
+for(j in 1:504){
+  X_li_train[[j]] = u_dsample(X_li_train[[j]])
+}
+
+for(j in 1:150){
+  X_li_test[[j]] = u_dsample(X_li_test[[j]])
+}
+
+# classification 
+
+X = rep(0, J*k)
+X_test = rep(0, J*k)
+for(i in 1:n_train_total){
+  if(i <= n_train_1){
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  } else{
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  }
+}
+
+X = X[-1,]
+
+df = cbind(X, Y)
+df = as.data.frame(df)
+df$Y = as.factor(df$Y)
+DWT = qda_diag(Y~., df)
+
+for(i in 1:n_test_total){
+  if(i <= n_test_1){
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  } else{
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  }
+}
+X_test = X_test[-1,]
+df_test = as.data.frame(X_test)
+y_hat_c3 = predict(DWT, df_test)
+print(paste("Channel C3 accuracy:", round(sum(predict(DWT, df_test) == Y_test)/n_test_total, 3)))
+
+
+# C4 train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_C4.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_C4.rds")
+
+for(j in 1:504){
+  X_li_train[[j]] = u_dsample(X_li_train[[j]])
+}
+
+for(j in 1:150){
+  X_li_test[[j]] = u_dsample(X_li_test[[j]])
+}
+
+# classification  
+X = rep(0, J*k)
+X_test = rep(0, J*k)
+for(i in 1:n_train_total){
+  if(i <= n_train_1){
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  } else{
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  }
+}
+
+X = X[-1,]
+
+df = cbind(X, Y)
+df = as.data.frame(df)
+df$Y = as.factor(df$Y)
+DWT = qda_diag(Y~., df)
+
+for(i in 1:n_test_total){
+  if(i <= n_test_1){
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  } else{
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  }
+}
+X_test = X_test[-1,]
+df_test = as.data.frame(X_test)
+y_hat_c4 = predict(DWT, df_test)
+print(paste("Channel C4 accuracy:", round(sum(predict(DWT, df_test) == Y_test)/n_test_total, 3)))
+
+
+
+# CZ train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_CZ.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_CZ.rds")
+
+for(j in 1:504){
+  X_li_train[[j]] = u_dsample(X_li_train[[j]])
+}
+
+for(j in 1:150){
+  X_li_test[[j]] = u_dsample(X_li_test[[j]])
+}
+# classification  
+X = rep(0, J*k)
+X_test = rep(0, J*k)
+for(i in 1:n_train_total){
+  if(i <= n_train_1){
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  } else{
+    ts = X_li_train[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X = rbind(X, c(V_1, V_2))
+  }
+}
+
+X = X[-1,]
+
+df = cbind(X, Y)
+df = as.data.frame(df)
+df$Y = as.factor(df$Y)
+DWT = qda_diag(Y~., df)
+
+for(i in 1:n_test_total){
+  if(i <= n_test_1){
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  } else{
+    ts = X_li_test[[i]]
+    W_1 = wavelets::modwt(ts[1:(n/k)], n.levels = J)
+    W_2 = wavelets::modwt(ts[(n/k + 1):n], n.levels = J) 
+    L_j = W_1@n.boundary
+    W_1 = W_1@W
+    W_2 = W_2@W
+    V_1 = c()
+    V_2 = c()
+    for(j in 1:J){
+      M = N - L_j[j] + 1
+      V_1[j] = sum((as.vector(W_1[[j]])[(L_j[j]-1):(N-1)])^2)/M
+      V_2[j] = sum((as.vector(W_2[[j]])[(L_j[j]-1):(N-1)])^2)/M
+    }
+    X_test = rbind(X_test, c(V_1, V_2))
+  }
+}
+X_test = X_test[-1,]
+df_test = as.data.frame(X_test)
+y_hat_cz = predict(DWT, df_test)
+print(paste("Channel CZ accuracy:", round(sum(predict(DWT, df_test) == Y_test)/n_test_total, 3)))
+
+
+# Majority voting 
+preds_matrix <- cbind(as.numeric(y_hat_cz), 
+                      as.numeric(y_hat_c3), 
+                      as.numeric(y_hat_c4))
+
+# 2. Perform Majority Voting
+# Since your levels are 1 and 2:
+# We count how many times '1' appears in each row.
+# If '1' appears 2 or more times, the winner is 1. Otherwise, it is 2.
+votes_for_1 <- rowSums(preds_matrix == 1)
+majority <- as.factor(ifelse(votes_for_1 >= 2, 1, 2))
+
+print(paste("Majority voting accuracy:", round(sum(majority == Y_test)/150, 3)))
+
+
---
+++ Table 2/Proposed_realdata.R
@@ -0,0 +1,162 @@
+
+
+#  classfication for EEG 
+
+# C3 train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_C3.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_C3.rds")
+
+# classification  
+ma_diff_train = c()
+bc_list_train_fast = list()
+bc_list_test_fast = list()
+for(num_obs in 1:504){
+  res = auto.fit.legen_fast(X_li_train[[num_obs]], c(1:10), c(1:4), method = "LOOCV")
+  bc_list_train_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff_train[num_obs] = max(phi_j) - min(phi_j)
+}
+Y = c(rep("abnormal", 504/2), rep("normal", 504/2))
+diff = seq(1/2*min(ma_diff_train), 2*max(ma_diff_train), length.out = 10000)
+result = c()
+for(j in 1:10000){
+  y_pre = rep(0, 504)
+  for(num_obs in 1:504){
+    y_pre[num_obs] = ifelse(ma_diff_train[num_obs] > diff[j], "abnormal", "normal")
+  }
+  result[j] = sum(y_pre == Y)
+}
+threshold = diff[which.max(result)]
+# test 
+ma_diff = c()
+for(num_obs in 1:length(X_li_test)){
+  res = auto.fit.legen_fast(X_li_test[[num_obs]], c(1:11), c(1:3), method = "LOOCV", inte = T)
+  bc_list_test_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff[num_obs] = max(phi_j) - min(phi_j)
+}
+result = c() 
+Y = c(rep("abnormal", length(X_li_test)/2), rep("normal", length(X_li_test)/2))
+y_pre = rep(0, length(X_li_test))
+for(num_obs in 1:length(X_li_test)){
+  y_pre[num_obs] = ifelse(ma_diff[num_obs] > threshold, "abnormal", "normal")
+}
+result = sum(y_pre == Y)/length(X_li_test)
+y_hat_c3 = y_pre
+
+print(paste("Channel C3 accuracy:", round(result, 3)))
+
+
+
+
+# C4 train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_C4.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_C4.rds")
+
+# classification  
+ma_diff_train = c()
+bc_list_train_fast = list()
+bc_list_test_fast = list()
+for(num_obs in 1:504){
+  res = auto.fit.legen_fast(X_li_train[[num_obs]], c(1:10), c(1:4), method = "LOOCV")
+  bc_list_train_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff_train[num_obs] = max(phi_j) - min(phi_j)
+}
+Y = c(rep("abnormal", 504/2), rep("normal", 504/2))
+diff = seq(1/2*min(ma_diff_train), 2*max(ma_diff_train), length.out = 10000)
+result = c()
+for(j in 1:10000){
+  y_pre = rep(0, 504)
+  for(num_obs in 1:504){
+    y_pre[num_obs] = ifelse(ma_diff_train[num_obs] > diff[j], "abnormal", "normal")
+  }
+  result[j] = sum(y_pre == Y)
+}
+threshold = diff[which.max(result)]
+# test 
+ma_diff = c()
+for(num_obs in 1:length(X_li_test)){
+  res = auto.fit.legen_fast(X_li_test[[num_obs]], c(1:11), c(1:3), method = "LOOCV")
+  bc_list_test_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff[num_obs] = max(phi_j) - min(phi_j)
+}
+result = c() 
+Y = c(rep("abnormal", length(X_li_test)/2), rep("normal", length(X_li_test)/2))
+y_pre = rep(0, length(X_li_test))
+for(num_obs in 1:length(X_li_test)){
+  y_pre[num_obs] = ifelse(ma_diff[num_obs] > threshold, "abnormal", "normal")
+}
+result = sum(y_pre == Y)/length(X_li_test)
+y_hat_c4 = y_pre
+print(paste("Channel C4 accuracy:", round(result, 3)))
+
+
+
+
+# CZ train set and test set 
+X_li_train = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/train_set/train_CZ.rds")
+X_li_test = readRDS("/Users/chenqian/Desktop/Real_Data(with noise)/test_set/test_CZ.rds")
+
+# classification  
+ma_diff_train = c()
+bc_list_train_fast = list()
+bc_list_test_fast = list()
+for(num_obs in 1:504){
+  res = auto.fit.legen_fast(X_li_train[[num_obs]], c(1:10), c(1:4), method = "LOOCV")
+  bc_list_train_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff_train[num_obs] = max(phi_j) - min(phi_j)
+}
+Y = c(rep("abnormal", 504/2), rep("normal", 504/2))
+diff = seq(1/2*min(ma_diff_train), 2*max(ma_diff_train), length.out = 10000)
+result = c()
+for(j in 1:10000){
+  y_pre = rep(0, 504)
+  for(num_obs in 1:504){
+    y_pre[num_obs] = ifelse(ma_diff_train[num_obs] > diff[j], "abnormal", "normal")
+  }
+  result[j] = sum(y_pre == Y)
+}
+threshold = round(diff[which.max(result)], 2)
+# test 
+ma_diff = c()
+for(num_obs in 1:length(X_li_test)){
+  res = auto.fit.legen_fast(X_li_test[[num_obs]], c(1:10), c(1:3), method = "LOOCV", inte = T)
+  bc_list_test_fast[[num_obs]] = res[[4]]
+  res_coef = res[[1]]
+  phi_j = res_coef[[length(res_coef)]]
+  ma_diff[num_obs] = max(phi_j) - min(phi_j)
+}
+result = c() 
+Y = c(rep("abnormal", length(X_li_test)/2), rep("normal", length(X_li_test)/2))
+y_pre = rep(0, length(X_li_test))
+for(num_obs in 1:length(X_li_test)){
+  y_pre[num_obs] = ifelse(ma_diff[num_obs] > threshold, "abnormal", "normal")
+}
+result = sum(y_pre == Y)/length(X_li_test)
+y_hat_cz = y_pre
+
+print(paste("Channel CZ accuracy:", round(result, 3)))
+
+
+# Majority voting 
+# 1. Combine vectors into a matrix
+matrix_preds <- cbind(as.character(y_hat_cz), 
+                      as.character(y_hat_c3), 
+                      as.character(y_hat_c4))
+# 2. Compute Majority
+# Count how many classifiers predicted "abnormal" for each row
+votes_abnormal <- rowSums(matrix_preds == "abnormal")
+# If "abnormal" has 2 or more votes, the majority is "abnormal". Otherwise, "normal".
+majority <- ifelse(votes_abnormal >= 2, "abnormal", "normal")
+print(paste("Majority voting accuracy:", round(sum(majority == Y)/150, 3)))
+
+
+
