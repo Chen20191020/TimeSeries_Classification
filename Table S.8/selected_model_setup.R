44711ced6734fba9b7cb30efcbeeaafd9f577fa2
Chen20191020 <57544179+Chen20191020@users.noreply.github.com>
2026-02-19 15:10:34 -0800

    Update selected_model_setup.R

--- Table S.8/selected_model_setup.R
+++ Table S.8/selected_model_setup.R
@@ -67,76 +67,76 @@ generate_AR2_class2 = function(n, v){
 
 # Model a
 
-generate_AR2s2c1_class1 = function(n, v){
-  ts = c()
-  w = rnorm(n+2, 0, 1/v)
+#generate_AR2s2c1_class1 = function(n, v){
+#  ts = c()
+#  w = rnorm(n+2, 0, 1/v)
   
-  for(i in 1:n){
-    if(i == 1){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.4*sin(2*pi*(i/n))*w[2] + w[i+2]
-    } else if(i == 2){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    } else {
-      ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    }
-  }
-  return(ts)
-}
+# for(i in 1:n){
+#   if(i == 1){
+#     ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.4*sin(2*pi*(i/n))*w[2] + w[i+2]
+#   } else if(i == 2){
+#     ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#   } else {
+#     ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    }
+#  }
+#  return(ts)
+#}
 
-generate_AR2s2c1_class2 = function(n, v){
-  ts = c()
-  w = rnorm(n+2, 0, 1/v)
-  
-  for(i in 1:n){
-    if(i == 1){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.6*sin(2*pi*(i/n))*w[2] + w[i+2]
-    } else if(i == 2){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    } else {
-      ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    }
-  }
-  return(ts)
-}
+#generate_AR2s2c1_class2 = function(n, v){
+#  ts = c()
+#  w = rnorm(n+2, 0, 1/v)
+#  
+#  for(i in 1:n){
+#    if(i == 1){
+#      ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.6*sin(2*pi*(i/n))*w[2] + w[i+2]
+#    } else if(i == 2){
+#      ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    } else {
+#      ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    }
+#  }
+#  return(ts)
+#}
 
 
 
 # Model b
 
-generate_AR2pro_class1 = function(n, v){
-  ts = c()
-  w = rnorm(n+2, 0, 1/v)
-  
-  for(i in 1:n){
-    if(i == 1){
-      ts[i] =  0.6*cos(2*pi*(i/n))*w[1] + 0.4*sin(2*pi*(i/n))*w[2] + w[i+2]
-    } else if(i == 2){
-      ts[i] =  0.6*cos(2*pi*(i/n))*w[2]  + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    } else {
-      ts[i] =  0.6*cos(2*pi*(i/n))*ts[i-2] + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    }
-  }
-  return(ts)
-}
+#generate_AR2pro_class1 = function(n, v){
+#  ts = c()
+#  w = rnorm(n+2, 0, 1/v)
+#  
+#  for(i in 1:n){
+#    if(i == 1){
+#      ts[i] =  0.6*cos(2*pi*(i/n))*w[1] + 0.4*sin(2*pi*(i/n))*w[2] + w[i+2]
+#    } else if(i == 2){
+#      ts[i] =  0.6*cos(2*pi*(i/n))*w[2]  + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    } else {
+#      ts[i] =  0.6*cos(2*pi*(i/n))*ts[i-2] + 0.4*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    }
+#  }
+#  return(ts)
+#}
 
-generate_AR2pro_class2 = function(n, v){
-  ts = c()
-  w = rnorm(n+2, 0, 1/v)
-  
-  for(i in 1:n){
-    if(i == 1){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.6*sin(2*pi*(i/n))*w[2] + w[i+2]
-    } else if(i == 2){
-      ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    } else {
-      ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
-    }
-  }
-  return(ts)
-}
+#generate_AR2pro_class2 = function(n, v){
+#  ts = c()
+#  w = rnorm(n+2, 0, 1/v)
+#  
+#  for(i in 1:n){
+#    if(i == 1){
+#      ts[i] =  0.4*cos(2*pi*(i/n))*w[1] + 0.6*sin(2*pi*(i/n))*w[2] + w[i+2]
+#    } else if(i == 2){
+#      ts[i] =  0.4*cos(2*pi*(i/n))*w[2]  + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    } else {
+#      ts[i] =  0.4*cos(2*pi*(i/n))*ts[i-2] + 0.6*sin(2*pi*(i/n))*ts[i-1] + w[i+2]
+#    }
+#  }
+#  return(ts)
+#}
 
 
-# Model c
+# Model 18
 
 generate_AR1s_class1 = function(n, v){
   ts = c()
@@ -167,8 +167,14 @@ generate_AR1s_class2 = function(n, v){
 function_names <- c(
   "generate_AR1_class1", "generate_AR1_class2",
   "generate_AR2_class1", "generate_AR2_class2",
-  "generate_AR2s2c1_class1", "generate_AR2s2c1_class2",
-  "generate_AR2pro_class1", "generate_AR2pro_class2", 
   "generate_AR1s_class1", "generate_AR1s_class2"
 )
+
+#function_names <- c(
+#  "generate_AR1_class1", "generate_AR1_class2",
+#  "generate_AR2_class1", "generate_AR2_class2",
+#  "generate_AR2s2c1_class1", "generate_AR2s2c1_class2",
+#  "generate_AR2pro_class1", "generate_AR2pro_class2", 
+#  "generate_AR1s_class1", "generate_AR1s_class2"
+#)
 
